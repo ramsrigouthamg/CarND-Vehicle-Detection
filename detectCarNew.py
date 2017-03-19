cc627ee4a2ceaf5e7c90fe08ccfa536d9f7d947f
RAMSRIG\Ramsri <ramsrigouthamg@gmail.com>
2017-03-19 15:19:25 -0700

    Added heat map !

--- detectCarNew.py
+++ detectCarNew.py
@@ -19,6 +19,37 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     # Return the image copy with boxes drawn
     return imcopy
 
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
 # Define a function to return HOG features and visualization
 def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                         vis=False, feature_vec=True):
@@ -113,6 +144,7 @@ if __name__ == "__main__":
 
 
     image = mpimg.imread('test_images/test1.jpg')
+    heat = np.zeros_like(image[:, :, 0]).astype(np.float)
     ystart = 400
     ystop = 656
     scale = 1.0
@@ -123,6 +155,28 @@ if __name__ == "__main__":
     X_scaler = pickle.load(open("saved_X_scaler_YCrCb.p", "rb"))  # Load svc
 
     out_img,bboxes = find_cars(image, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block,showImage=False)
-    finalImage = draw_boxes(image,bboxes)
-    plt.imshow(finalImage)
+    finalImage = draw_boxes(image,bboxes,color=(0, 0, 255))
+
+    # Add heat to each box in box list
+    heat = add_heat(heat, bboxes)
+    # Apply threshold to help remove false positives
+    heat = apply_threshold(heat, 2)
+
+    # Visualize the heatmap when displaying
+    heatmap = np.clip(heat, 0, 255)
+
+    # Find final boxes from heatmap using label function
+    labels = label(heatmap)
+    draw_img = draw_labeled_bboxes(np.copy(image), labels)
+
+    fig = plt.figure()
+    plt.subplot(121)
+    plt.imshow(draw_img)
+    plt.title('Car Positions')
+    plt.subplot(122)
+    plt.imshow(heatmap, cmap='hot')
+    plt.title('Heat Map')
+    fig.tight_layout()
+
+    # plt.imshow(finalImage)
     plt.show()
